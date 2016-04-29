b63fc6f4d33c4f00202b10d7074d4f57533b754a
Jorge Roldán <jorge.roldan@gmail.com>
2016-04-29 19:50:53 +0200

    Initial commit

---
+++ src/main/java/com/roldan/currencyconverter/LoginController.java
@@ -0,0 +1,17 @@
+package com.roldan.currencyconverter;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class LoginController {
+
+	@RequestMapping(method=RequestMethod.GET)
+	public String home(Map<String,Object> model) {
+		return "home";
+	}
+}
